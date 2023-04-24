4bc414b380610462748c84726171b573ad651792
Saurabhkaran11 <saurabhkaran11@gmail.com>
2023-04-23 23:30:48 -0700

    Updated Deposit and withdraw card

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 // To get Transaction Type
     const transactType = document.querySelector("#transact-type");
 
-    console.log('Test1');
-    console.log(transactType);
+    // console.log('Test1');
+    // console.log(transactType);
 
     // Get Transaction Forms:
     const paymentCard = document.querySelector(".payment-card");
-    console.log('Test2');
-    console.log(paymentCard);
+    // console.log('Test2');
+    // console.log(paymentCard);
 
     const transferCard = document.querySelector(".transfer-card");
+    // console.log('Test3');
+    // console.log(transferCard);    
     const depositCard = document.querySelector(".deposit-card");
-    console.log('Test3');
-    console.log(transferCard);
+    const withdrawCard = document.querySelector(".withdraw-card");
+
 
     paymentCard.style.display = "none";
     transferCard.style.display = "none";
+    depositCard.style.display = "none";
+    withdrawCard.style.display = "none";
 
 
     transactType.addEventListener("change", function () {
@@ -36,10 +40,24 @@
           transferCard.style.display = "block";
           transferCard.nextElementSibling.style.display = "none";
           break;
-        
+
+        case "deposit":
+          depositCard.previousElementSibling.style.display = "none";
+          depositCard.style.display = "block";
+          depositCard.nextElementSibling.style.display = "none";
+          break;
+
+          case "withdraw":
+            withdrawCard.previousElementSibling.style.display = "none";
+            withdrawCard.style.display = "block";
+            withdrawCard.nextElementSibling.style.display = "none";
+            break;
+
         default:
           paymentCard.style.display = "none";
           transferCard.style.display = "none";
+          withdrawCard.style.display = "none";
+          depositCard.style.display = "none";
           break;
       }
     });
